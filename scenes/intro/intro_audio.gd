6c2cee353d872e52e17b57ed7382374a07f5a60d
Aleks Lime <alekslime330@gmail.com>
2026-05-13 02:41:25 +0200

    i dashur zot. te lutem me fal per ate audio qe kam krijuar per kete loje te mallkuar. (praying emoji) added the cult hum song when leaving the house, trying to add glowing sigils for the player to follow to the end of the level but so far its not working. audio works completely fine though. peak.

--- scenes/intro/intro_audio.gd
+++ scenes/intro/intro_audio.gd
@@ -11,6 +11,10 @@ var outside_triggered: bool = false
 func _ready() -> void:
 	music_warm.play()
 	ambient_outside.play()
+	# hide sigils at start
+	var sigils = get_node_or_null("Sigils")
+	if sigils:
+		sigils.visible = false
 
 func trigger_wife() -> void:
 	voice_wife.play()
@@ -20,6 +24,20 @@ func trigger_inside() -> void:
 	_fade_out(ambient_outside, 1.5)
 	_fade_out_3d(voice_wife, 0.5)
 	music_eerie.play()
+
+func trigger_outside() -> void:
+	if outside_triggered:
+		return
+	outside_triggered = true
+	_fade_out(music_eerie, 1.0)
+	music_classical.play()
+	# show sigils and start cult hum
+	var sigils = get_node_or_null("Sigils")
+	if sigils:
+		sigils.visible = true
+	var hum = get_node_or_null("IntroAudio/CultHum")
+	if hum:
+		hum.play()
 
 func _fade_out(player: AudioStreamPlayer, duration: float) -> void:
 	var tween = create_tween()
